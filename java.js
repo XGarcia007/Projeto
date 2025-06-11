3f7486bf2329efb7e00f4ab0ab3babdceb1bdb07
XGarcia007 <leonardo.garcia8@aluno.senai.br>
2025-06-11 07:28:00 -0300

    final

--- java.js
+++ java.js
@@ -1,44 +1,44 @@
 
-        function logar(){
+function logar() {
 
-            var login = document.getElementById('login').value;
-            var senha = document.getElementById('senha').value;
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
 
-            if(login == "admin"  && senha == "admin"){
-                alert('Sucesso');
-                location.href = "home.html";
-            }else{
-                alert('Usuario ou senha incorretos');
-            }
-            
-        }
+    if (login == "admin" && senha == "admin") {
+        alert('Sucesso');
+        location.href = "home.html";
+    } else {
+        alert('Usuario ou senha incorretos');
+    }
 
-       
-        
-        
-
-    function iniciarComentarios() {
- 
-        const form = document.getElementById("form-comentario");
-        const lista = document.getElementById("lista-comentarios");
-
-    form.addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    const nome = document.getElementById("nome").value;
-    const texto = document.getElementById("texto").value;
-    const nota = document.getElementById("nota").value;
-
-    const novoComentario = document.createElement("div");
-    novoComentario.innerHTML = `<p><strong>${nome}</strong> (${nota} ⭐):<br>${texto}</p><hr>`;
-
-    lista.appendChild(novoComentario);
-    form.reset();
-  });
 }
 
-function cadastrar(){
-    
+
+
+
+
+function iniciarComentarios() {
+
+    const form = document.getElementById("form-comentario");
+    const lista = document.getElementById("lista-comentarios");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const nome = document.getElementById("nome").value;
+        const texto = document.getElementById("texto").value;
+        const nota = document.getElementById("nota").value;
+
+        const novoComentario = document.createElement("div");
+        novoComentario.innerHTML = `<p><strong>${nome}</strong> (${nota} ⭐):<br>${texto}</p><hr>`;
+
+        lista.appendChild(novoComentario);
+        form.reset();
+    });
+}
+
+function cadastrar() {
+
 
 }
 
